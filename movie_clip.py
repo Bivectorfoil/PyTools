014ec759ade0e29de2741504513ac4ed95638f95
Bivectorfoil <550282600@qq.com>
2018-03-26 22:39:31 +0800

    A script to batch clip videos

---
+++ movie_clip.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+
+import os
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+
+'''
+A script to batch clip video
+'''
+
+path = raw_input("Enter the absolute path: ")  # absolute path
+directory = os.listdir(path)  # get the video files' name
+
+# start time, whatever u want, it's the beginning of the new cliped video
+# like this:
+# original-------------------------end
+# <--clipped part-->t1-------------end
+t1 = 0.0
+
+for f in directory:
+    clip_file_name = path + f  # the absolute path of the video file
+    print clip_file_name
+    # get the length of the video
+    # t2 can be whatever u want, it's the end of the new clipped video.
+    # as follow, it's the length of original video.
+    t2 = VideoFileClip(clip_file_name).duration
+    print t2
+    # absolte path, its name shuld NOT be the same as original video
+    tar_name = path + 'test_' + f
+    print tar_name
+    try:
+        ffmpeg_extract_subclip(clip_file_name, t1, t2,
+                               targetname=tar_name)
+    except Exception as e:
+        print e
